119e820e29e7561f131b15b373558096cca480ea
Kartikeya Srivastava <kartikeyasrivastava861@gmail.com>
2020-11-30 20:45:38 +0530

    Update Grabber.py
    updated with better regex for Indian phone numbers and filter.

--- Grabber.py
+++ Grabber.py
@@ -1,11 +1,10 @@
-import re              # this provide format that needs to be scraped
-import time            # it make the program to sleep for specific time
- 
-from urllib.request import Request, urlopen  # this get the html ,act like headless browser
+import re         
+import time
+from urllib.request import Request, urlopen 
 from bs4 import BeautifulSoup
-# print("ENTER THE URL TO FIND MOBILE NUMBER AND EMAIL url eg:https://www.homersbrandcare.com")     # url of website from which we need email and phone number
+
  
-url="https://www.resonance.ac.in/" #EXAMPLE URL because it has dynamic contact page
+url="http://www.resonance.ac.in"
 req = Request(url)
 html = urlopen(req).read()
 html=html.decode()
@@ -19,20 +18,21 @@ for link in bsObj.find_all('a'):
 def Email(url):
     try:
         req = Request(url)
-        hh = urlopen(req).read()       # we are making request act like firefox browser
+        hh = urlopen(req).read() 
         time.sleep(0.5)
         #time.sleep(1)
     #raw_html=t.text
     #dd=html2text.html2text(hh)
         dd=hh.decode()
-        email=re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+',dd)   # this is the format ,based on this format only we will get results
-                                                                                                                    # this is email format
+        email=re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+',dd)   
+        email = list(dict.fromkeys(email))                                                                                                            # this is email format
         print(email)
-        phone=re.findall(r'\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})',dd)   # phone number format
-        print(phone)
-        # for ph in phone:            
-        #     print('-'.join(ph)," <-> ")
-        # print()
+        phone=re.findall(r'\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})',dd)  
+        #print(phone)
+        for ph in phone:
+            if(ph[0]=='' and (ph[1].startswith('09') or ph[1].startswith('9'))):
+                print(ph[1:4])
+     #  print()
     #links.remove(url)
     except:
        pass
